5a2213440986a965df46d3ca298c5b77b595fa15
Aodan <hardy-a4@ulster.ac.uk>
2024-08-20 16:39:19 +0100

    updating detail view

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Product
 from django.core.paginator import Paginator
+
+
 # Create your views here.
 
 
@@ -13,8 +15,13 @@ def index(request):
         product_objects = product_objects.filter(title__icontains=item_name)
 
     # Paginator
-    paginator = Paginator(product_objects, 3)
+    paginator = Paginator(product_objects, 16)
     page = request.GET.get('page')
     product_objects = paginator.get_page(page)
 
     return render(request, 'shop/index.html', {'product_objects': product_objects})
+
+
+def detail(request, id):
+    product_object = Product.objects.get(id=id)
+    return render(request, 'shop/detail.html', {'product_object': product_object})

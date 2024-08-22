59a80de3f21dcc3bd398f7a611cb21e7e5315d7b
Aodan <hardy-a4@ulster.ac.uk>
2024-08-22 13:56:12 +0100

    admin updates

--- shop/admin.py
+++ shop/admin.py
@@ -4,5 +4,23 @@ from .models import Product, Order
 # Register your models here.
 
 
-admin.site.register(Product)
+admin.site.site_header = "Ecommerce Site"
+admin.site.site_title = "Ecommerce"
+admin.site.index_title = "Manage Ecommerce"
+
+
+class ProductAdmin(admin.ModelAdmin):
+
+    def changeCategoryToDefault(self, queryset):
+        queryset.update(category='default')
+
+    changeCategoryToDefault.short_description = "Change category to default"
+
+    list_display = ('title', 'price', 'category', 'description')
+    search_fields = ('title', 'category')
+    actions = ('changeCategoryToDefault',)
+    list_editable = ('price',)
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
